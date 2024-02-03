ac8972ea9997926c19f9c0b4372faa059b993ee4
Isabella <isa021andrade@gmail.com>
2024-02-03 18:05:03 -0300

    Adicionadas funções de colorir (rgb, escala de cinza e preto)

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
 // 54 = 9 x 9
 // 
 const parent = document.querySelector("#grid-box")
+const btnRgb = document.querySelector("#btn-rgb")
+const btnBlack = document.querySelector("#btn-black")
+const btnProgressiveBlack = document.querySelector("#btn-progressive-black")
+const btnClear = document.querySelector("#btn-clear")
+randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
 
-for (let i = 0; i<90; i++){
+function paintRandomRGB() {
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    return `rgb(${r},${g},${b})`;
+}
+
+let opacity = 0.0
+function paintProgressiveBlack(){
+    opacity += 0.01
+    return `rgb(0,0,0,${opacity})`
+}
+
+function paintBlack(){
+    return `rgb(0,0,0)`
+}
+
+function clearScreen() {
+    document.querySelectorAll(".grid-item").forEach((child) =>
+    child.style.backgroundColor = "#e8e8e8"
+)}
+
+for (let i = 0; i<4096; i++){
     const grid = document.createElement("div");
     grid.className = "grid-item";
     parent.appendChild(grid);
@@ -11,6 +38,19 @@ for (let i = 0; i<90; i++){
 document.querySelectorAll(".grid-item").forEach((child) =>
     child.addEventListener("mouseover", () => changeColor(child)))
 
+//change color
+let colorType = 0
+btnRgb.addEventListener('click', ()=>colorType = 0)
+btnProgressiveBlack.addEventListener('click', ()=> {
+    opacity = 0.0
+    colorType = 1})
+btnBlack.addEventListener('click', ()=>colorType = 2)
+btnClear.addEventListener('click', ()=>this.clearScreen())
 function changeColor(child) {
-    child.style.backgroundColor = "green";
+    if(colorType == 0)
+        child.style.backgroundColor = paintRandomRGB();
+    else if(colorType == 1)
+        child.style.backgroundColor = paintProgressiveBlack();
+    else 
+        child.style.backgroundColor = paintBlack();
 }
